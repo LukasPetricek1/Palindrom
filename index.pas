1c41e813905624e586a7a7f0e707ddab4e18ea42
LukasPetricek1 <126686136+LukasPetricek1@users.noreply.github.com>
2023-06-07 16:13:44 +0200

    Add files via upload

---
+++ index.pas
@@ -0,0 +1,37 @@
+program PalindromeChecker;
+
+uses
+  SysUtils;
+
+procedure Palindrome(const AText: string);
+var
+  WithSpaces, LengthNoSpaces: Integer;
+  Text: string;
+  Index: Integer;
+  Statement: Boolean;
+begin
+  WithSpaces := Length(AText);
+  Text := LowerCase(StringReplace(AText, ' ', '', [rfReplaceAll]));
+  LengthNoSpaces := Length(Text);
+
+  Statement := True;
+  for Index := 1 to Trunc(LengthNoSpaces / 2) do
+  begin
+    if not (Text[Index] = Text[LengthNoSpaces - Index + 1]) then
+    begin
+      Statement := False;
+      Break;
+    end;
+  end;
+
+  Writeln(statement, '   ', LengthNoSpaces, '  ( s mezerami: ', WithSpaces, ' )');
+end;
+
+var
+  Text: string;
+begin
+  Write('Napište palindrom: ');
+  Readln(Text);
+
+  Palindrome(Text);
+end.
